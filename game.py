e6b3919721105ced9d9dc7bf628ae2ea74173911
Guy <guysayfan@gmail.com>
2017-11-19 14:42:15 -0800

    Made rows of bricks instead of a single brick.

--- game.py
+++ game.py
@@ -1,12 +1,28 @@
 import pygame
 import pygame.constants
+import os
+
+os.environ['SDL_VIDEO_CENTERED'] = '1'
 
 black = (0, 0, 0)
-gray = (64, 64, 64)
+gray = (210, 210, 210)
 white = (255, 255, 255)
+cyan = (175, 231, 231)
+laser_red = (255, 36, 36)
 
-display_width = 800
-display_height = 600
+
+screen_width = 1364
+screen_height = 742
+
+left_down = False
+right_down = False
+
+paddle_width = 88
+paddle_height = 7
+paddle_left = (screen_width // 2) - (paddle_width // 2)
+paddle_top = screen_height - 50
+paddle_movement = 17
+
 ball_radius = 10
 plus_key = 270
 minus_key = 269
@@ -15,15 +31,15 @@ brick_width = 50
 brick_height = 30
 
 pygame.init()
-screen = pygame.display.set_mode((display_width, display_height))
+screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Brick Break')
 clock = pygame.time.Clock()
 gameover = False
 
 row_count = 5
 
-brick_count = display_width // (brick_width + 1)
-gap = display_width - brick_count * (brick_width + 1)
+brick_count = screen_width // (brick_width + 1)
+gap = screen_width - brick_count * (brick_width + 1)
 offset = gap // 2
 
 
@@ -35,21 +51,41 @@ def draw_brick_rows():
     for i in range(row_count):
         draw_brick_row((brick_height + 1) * i)
 
+def draw_paddle():
+    pygame.draw.rect(screen, cyan, (paddle_left, paddle_top, paddle_width, paddle_height))
+
 
 while not gameover:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             gameover = True
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                left_down = True
+            if event.key == pygame.K_RIGHT:
+                right_down = True
         if event.type == pygame.KEYUP:
-            if event.key == plus_key:
-                ball_radius = ball_radius + 1
-            if event.key == minus_key:
-                ball_radius = ball_radius - 1
+            if event.key == pygame.K_LEFT:
+                left_down = False
+            if event.key == pygame.K_RIGHT:
+                right_down = False
+
+    # Move paddle if needed
+    movement_left = min(paddle_movement, paddle_left - offset)
+    paddle_right = paddle_left + paddle_width
+    right_wall = screen_width - offset
+    movement_right = min(paddle_movement, right_wall - paddle_right)
+    if left_down:
+        paddle_left = paddle_left - movement_left
+    if right_down:
+        paddle_left = paddle_left + movement_right
+
 
     # Drawing
     screen.fill(white)
     draw_brick_rows()
-    pygame.draw.circle(screen, gray, (display_width // 2, display_height // 2), ball_radius)
+    pygame.draw.circle(screen, gray, (screen_width // 2, screen_height // 2), ball_radius)
+    draw_paddle()
 
     pygame.display.update()
     clock.tick(60)
